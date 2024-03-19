8f810f74d061bcdff14092ee6619f1857b775579
gkemzm <gkemzm@gmail.com>
2024-03-20 00:48:42 +0900

    fix : ui 일부변경

--- src/component/technique.jsx
+++ src/component/technique.jsx
@@ -3,14 +3,18 @@ import { useState } from 'react';
 import styled from 'styled-components';
 
 const stackImage = [
-  { src: '/images/stack/js.png', name: 'JavaScript', width: 48 },
+  {
+    src: '/images/stack/js.png',
+    name: 'JavaScript',
+    width: 48,
+  },
   { src: '/images/stack/type.png', name: 'TypeScript', width: 48 },
   { src: '/images/stack/react.jpg', name: 'React', width: 48 },
   { src: '/images/stack/next.png', name: 'Next.js', width: 48 },
   { src: '/images/stack/graphql.png', name: 'GraphQl', width: 48 },
   { src: '/images/stack/apollo.png', name: 'apollo Client', width: 48 },
   { src: '/images/stack/emotion.png', name: 'Emotion', width: 48 },
-  { src: '/images/stack/recoil.png', name: 'Recoil', width: 24 },
+  { src: '/images/stack/recoil.png', name: 'Recoil', width: 48 },
   { src: '/images/stack/rest.png', name: 'RESTful Api', width: 48 },
   { src: '/images/stack/styled.jpg', name: 'Styled Component', width: 48 },
   { src: '/images/stack/jotai.jpg', name: 'Jotai', width: 48 },
@@ -21,7 +25,11 @@ const cooperationImage = [
   { src: '/images/cooperation/jira.jpg', name: 'Jira', width: 48 },
   { src: '/images/cooperation/redmine.png', name: 'Red Mine', width: 64 },
   { src: '/images/cooperation/postman.png', name: 'Postman', width: 48 },
-  { src: '/images/cooperation/playground.png', name: 'Play ground', width: 48 },
+  {
+    src: '/images/cooperation/playground.png',
+    name: 'graphQl Play ground',
+    width: 48,
+  },
   { src: '/images/cooperation/swagger.png', name: 'Swagger', width: 48 },
   { src: '/images/cooperation/storybook.png', name: 'Stroy Book', width: 48 },
   { src: '/images/cooperation/figma.png', name: 'Figma', width: 48 },
@@ -109,6 +117,12 @@ const Title = styled.div`
 
 const StackBox = styled.div`
   margin-bottom: 40px;
+
+  padding-bottom: 18vh;
+
+  @media screen and (max-width: 768px) {
+    padding: 0px;
+  }
 `;
 
 const ContentWrapper = styled.div`
